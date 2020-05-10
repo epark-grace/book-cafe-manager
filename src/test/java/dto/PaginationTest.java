0ba6d330cfe624b1c637060d6ea7b438f062b05c
E.Park <epark.grace@gmail.com>
2020-05-11 04:12:21 +0900

    test: Pagination 첫 페이지 계산 테스트 코드 추가

--- src/test/java/dto/PaginationTest.java
+++ src/test/java/dto/PaginationTest.java
@@ -12,7 +12,7 @@ public class PaginationTest {
     @ParameterizedTest(name = "총 게시글 수: {0}, 총 페이지 수: {1}")
     @CsvSource({"42, 5", "150, 15", "30, 3"})
     void getTotalPage_generalCase(int totalPost, int totalPage) {
-        Pagination pagination = new Pagination(totalPost);
+        Pagination pagination = new Pagination(totalPost, 1);
         assertEquals(totalPage, pagination.getTotalPage());
     }
 
@@ -20,9 +20,24 @@ public class PaginationTest {
     @ParameterizedTest(name = "총 게시글 수: {0}, 총 페이지 수: {1}")
     @CsvSource({"0, 1", "1, 1", "10, 1"})
     void getTotalPost_totalPostIsPostLimitOrBelow_ReturnOne(int totalPost, int totalPage) {
-        Pagination pagination = new Pagination(totalPost);
+        Pagination pagination = new Pagination(totalPost, 1);
         assertEquals(totalPage, pagination.getTotalPage());
     }
 
+    @DisplayName("일반적인 경우의 화면에 노출되는 첫 페이지 번호 계산")
+    @ParameterizedTest(name = "총 게시글 수: {0}, 현재 페이지 번호: {1}, 첫 페이지 번호: {2}")
+    @CsvSource({"200, 15, 11", "353, 34, 31", "400, 31, 31"})
+    void getFirstPage_generalCase(int totalPost, int currentPage, int firstPage) {
+        Pagination pagination = new Pagination(totalPost, currentPage);
+        assertEquals(firstPage, pagination.getFirstPage());
+    }
+
+    @DisplayName("현재 페이지가 화면에 노출되는 마지막 페이지인 경우 화면에 노출되는 첫 페이지 번호 계산")
+    @ParameterizedTest(name = "현재 페이지 번호: {1}, 첫 페이지 번호: {2}")
+    @CsvSource({"200, 20, 11", "300, 30, 21"})
+    void getFirstPage_currentPageIsLastPage(int totalPost, int currentPage, int firstPage) {
+        Pagination pagination = new Pagination(totalPost, currentPage);
+        assertEquals(firstPage, pagination.getFirstPage());
+    }
 
 }
