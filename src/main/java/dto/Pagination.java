da9c9a7792b0f6481020d36aba877dd78e913f73
E.Park <epark.grace@gmail.com>
2020-05-11 04:11:12 +0900

    feat: Pagination 첫 페이지 계산 추가

--- src/main/java/dto/Pagination.java
+++ src/main/java/dto/Pagination.java
@@ -3,15 +3,24 @@ package dto;
 public class Pagination {
 
     private final int POST_LIMIT = 10;
+    private final int PAGE_LIMIT = 10;
     private int totalPost;
     private int totalPage;
+    private int currentPage;
+    private int firstPage;
 
-    public Pagination(int totalPost) {
+    public Pagination(int totalPost, int currentPage) {
         this.totalPost = totalPost;
+        this.currentPage = currentPage;
         this.totalPage = (this.totalPost - 1) / POST_LIMIT + 1;
+        this.firstPage = (this.currentPage - 1) / PAGE_LIMIT * PAGE_LIMIT + 1;
     }
 
     public int getTotalPage() {
-        return this.totalPage;
+        return totalPage;
+    }
+
+    public int getFirstPage() {
+        return firstPage;
     }
 }
