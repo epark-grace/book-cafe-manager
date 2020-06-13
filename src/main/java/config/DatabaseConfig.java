7fdf1060e3f68dc238f604c6c04b3d99915fd3ca
E.Park <epark.grace@gmail.com>
2020-06-13 10:10:35 +0900

    feat: 스프링 설정 클래스 추가

---
+++ src/main/java/config/DatabaseConfig.java
@@ -0,0 +1,36 @@
+package config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig implements TransactionManagementConfigurer {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig("/application.properties");
+        HikariDataSource dataSource = new HikariDataSource(config);
+        return dataSource;
+    }
+
+    @Override
+    public TransactionManager annotationDrivenTransactionManager() {
+        return transactionManager();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+}
