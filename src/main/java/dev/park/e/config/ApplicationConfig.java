0fb00bfc768efa8cc45cebfe1ebd99d4d584d0c8
E.Park <epark.grace@gmail.com>
2020-06-18 03:39:59 +0900

    refactor: 패키지명 dev.park.e로 변경

--- src/main/java/dev/park/e/config/ApplicationConfig.java
+++ src/main/java/dev/park/e/config/ApplicationConfig.java
@@ -1,9 +1,9 @@
-package config;
+package dev.park.e.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"service", "dao"})
+@ComponentScan(basePackages = {"dev.park.e.service", "dev.park.e.dao"})
 public class ApplicationConfig {
 }
--- src/test/java/dev/park/e/dto/PaginationTest.java
+++ src/test/java/dev/park/e/dto/PaginationTest.java
@@ -1,4 +1,4 @@
-package dto;
+package dev.park.e.dto;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
