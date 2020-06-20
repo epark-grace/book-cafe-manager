31589d1869320cbd5fdc30d69b44df55ab77f7e4
E.Park <epark.grace@gmail.com>
2020-06-20 09:23:13 +0900

    refactor: 불필요한 import 삭제
    - TransactionManager 삭제

--- src/main/java/dev/park/e/config/DatabaseConfig.java
+++ src/main/java/dev/park/e/config/DatabaseConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
