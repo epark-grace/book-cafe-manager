e5c6b23991003faba85673dcae2d3694ef9a3bac
E.Park <epark.grace@gmail.com>
2020-06-30 20:11:26 +0900

    feat: ViewController.bookList() pagination 모델 추가

--- src/main/java/dev/park/e/controller/ViewController.java
+++ src/main/java/dev/park/e/controller/ViewController.java
@@ -1,11 +1,21 @@
 package dev.park.e.controller;
 
+import dev.park.e.dto.Pagination;
+import dev.park.e.service.PaginationService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ViewController {
+
+    private PaginationService paginationService;
+
+    public ViewController(PaginationService paginationService) {
+        this.paginationService = paginationService;
+    }
+
     @GetMapping("/")
     public String main() {
         return "index";
@@ -17,5 +27,9 @@ public class ViewController {
     }
 
     @GetMapping("/book-list/{currentPage}")
-    public String bookList(@PathVariable(name = "currentPage") String page) { return "book_list"; }
+    public String bookList(@PathVariable(name = "currentPage") int currentPage, Model model) {
+        Pagination pagination = paginationService.getPagination(currentPage);
+        model.addAttribute("pagination", pagination);
+        return "book_list";
+    }
 }
