930ecb5929149774846b11bb48c09f590668f191
E.Park <epark.grace@gmail.com>
2020-07-06 01:09:17 +0900

    feat: BookService.addBooks() 추가

---
+++ src/main/java/dev/park/e/service/BookService.java
@@ -0,0 +1,23 @@
+package dev.park.e.service;
+
+import dev.park.e.dao.BookDao;
+import dev.park.e.dto.Book;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    BookDao bookDao;
+
+    public BookService(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @Transactional
+    public int addBooks(List<Book> books) {
+        return bookDao.insertBook(books);
+    }
+}
