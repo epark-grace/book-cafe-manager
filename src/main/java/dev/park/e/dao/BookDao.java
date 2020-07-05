737f9923f01cabde36ff05e8b75ba5990998099c
E.Park <epark.grace@gmail.com>
2020-07-06 01:06:29 +0900

    feat: BookDao.insertBooks() 추가

---
+++ src/main/java/dev/park/e/dao/BookDao.java
@@ -0,0 +1,21 @@
+package dev.park.e.dao;
+
+import dev.park.e.dto.Book;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BookDao {
+
+    SqlSession sqlSession;
+
+    public BookDao(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    public int insertBook(List<Book> books) {
+        return sqlSession.insert("dev.park.e.mapper.BookMapper.insertBook", books);
+    }
+}
