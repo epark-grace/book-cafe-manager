0730beeefcadaaa2f881988d3cc7d665e81ba138
E.Park <epark.grace@gmail.com>
2020-07-09 13:02:43 +0900

    feat: BookMapper @Repository 추가

--- src/main/java/dev/park/e/mapper/BookMapper.java
+++ src/main/java/dev/park/e/mapper/BookMapper.java
@@ -1,9 +1,11 @@
 package dev.park.e.mapper;
 
 import dev.park.e.dto.Book;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookMapper {
     int selectBookCount();
     int insertBook(List<Book> book);
