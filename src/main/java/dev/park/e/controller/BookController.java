e91b64d04c14659434460c3192cf088f5202aa89
E.Park <epark.grace@gmail.com>
2020-07-09 13:04:33 +0900

    feat: BookController.addBooks() 리턴타입 List<Book>으로 변경

--- src/main/java/dev/park/e/controller/BookController.java
+++ src/main/java/dev/park/e/controller/BookController.java
@@ -16,7 +16,7 @@ public class BookController {
     }
 
     @PostMapping("api/books")
-    public int addBooks(@RequestBody List<Book> books) {
+    public List<Book> addBooks(@RequestBody List<Book> books) {
         return bookService.addBooks(books);
     }
 
