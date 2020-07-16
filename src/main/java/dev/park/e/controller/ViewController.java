959f51ecd659facb9605785193234307a43b5da2
E.Park <epark.grace@gmail.com>
2020-07-17 04:40:58 +0900

    feat: ViewController.bookList() 검색조건 파라미터 유무에 따라 전체도서/검색도서 구분 추가
    - 페이징, 서비스 메소드 분류
    - 검색 요청인 경우 모델에 쿼리스트링 추가

--- src/main/java/dev/park/e/controller/ViewController.java
+++ src/main/java/dev/park/e/controller/ViewController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewController {
@@ -26,9 +27,23 @@ public class ViewController {
     }
 
     @GetMapping("/book-list/{currentPage}")
-    public String bookList(@PathVariable(name = "currentPage") int currentPage, Model model) {
-        model.addAttribute("pagination", bookService.getPagination(currentPage));
-        model.addAttribute("bookList", bookService.getBookList(currentPage));
+    public String bookList(@PathVariable(name = "currentPage") int currentPage,
+                           @RequestParam(name = "title", required = false) String title,
+                           @RequestParam(name = "author", required = false) String author,
+                           Model model) {
+
+        if (title != null) {
+            model.addAttribute("bookList", bookService.searchBook("title", title, currentPage));
+            model.addAttribute("pagination", bookService.getPagination(currentPage, "title", title));
+            model.addAttribute("search", "?title=" + title);
+        } else if (author != null) {
+            model.addAttribute("bookList", bookService.searchBook("author", author, currentPage));
+            model.addAttribute("pagination", bookService.getPagination(currentPage, "author", author));
+            model.addAttribute("search", "?author=" + author);
+        } else {
+            model.addAttribute("bookList", bookService.getBookList(currentPage));
+            model.addAttribute("pagination", bookService.getPagination(currentPage));
+        }
         return "book_list";
     }
 }
