7cb579dea08efe0c3aa423d55c00a7d2759bcf39
E.Park <epark.grace@gmail.com>
2020-07-17 15:47:21 +0900

    refactor: ViewController.bookList() 검색조건 파라미터 HashMap으로 받도록 수정
    - 파라미터 HashMap으로 전체도서/도서검색/검색기준 구분하여 중복코드 줄임

--- src/main/java/dev/park/e/controller/ViewController.java
+++ src/main/java/dev/park/e/controller/ViewController.java
@@ -1,11 +1,16 @@
 package dev.park.e.controller;
 
+import dev.park.e.dto.Book;
+import dev.park.e.dto.Pagination;
 import dev.park.e.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Controller
 public class ViewController {
@@ -28,22 +33,35 @@ public class ViewController {
 
     @GetMapping("/book-list/{currentPage}")
     public String bookList(@PathVariable(name = "currentPage") int currentPage,
-                           @RequestParam(name = "title", required = false) String title,
-                           @RequestParam(name = "author", required = false) String author,
+                           @RequestParam HashMap<String, String> params,
                            Model model) {
 
-        if (title != null) {
-            model.addAttribute("bookList", bookService.searchBook("title", title, currentPage));
-            model.addAttribute("pagination", bookService.getPagination(currentPage, "title", title));
-            model.addAttribute("search", "?title=" + title);
-        } else if (author != null) {
-            model.addAttribute("bookList", bookService.searchBook("author", author, currentPage));
-            model.addAttribute("pagination", bookService.getPagination(currentPage, "author", author));
-            model.addAttribute("search", "?author=" + author);
+        List<Book> bookList;
+        Pagination pagination;
+
+        if (params.isEmpty()) {
+            bookList = bookService.getBookList(currentPage);
+            pagination = bookService.getPagination(currentPage);
         } else {
-            model.addAttribute("bookList", bookService.getBookList(currentPage));
-            model.addAttribute("pagination", bookService.getPagination(currentPage));
+            String column = "title";
+            if (params.get("author") != null) {
+                column = "author";
+            }
+
+            String keyword = params.get(column);
+
+            bookList = bookService.searchBook(currentPage, column, keyword);
+            pagination = bookService.getPagination(currentPage, column, keyword);
+
+            StringBuilder search = new StringBuilder("?");
+            search.append(column).append("=").append(keyword);
+
+            model.addAttribute("search", search.toString());
         }
+
+        model.addAttribute("bookList", bookList);
+        model.addAttribute("pagination", pagination);
+
         return "book_list";
     }
 }
