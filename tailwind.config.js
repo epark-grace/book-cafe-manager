ce8a168d90e922f1a52f0064432b7c97180b83e8
E.Park <epark.grace@gmail.com>
2020-07-23 23:53:30 +0900

    chore: tailwind.config.js grid-template-rows 도서목록 레이아웃 추가 (#12)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        'booklist': '1fr minmax(0, max-content) 1fr'
+      }
+    },
   },
   variants: {},
   plugins: [],
