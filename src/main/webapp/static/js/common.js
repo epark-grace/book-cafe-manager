d0b459986cda20a04bd4ac55f140c6fa32344d30
E.Park <epark.grace@gmail.com>
2020-08-07 22:56:09 +0900

    feat: common.js sendAjaxRequest() 추가 (#13)

---
+++ src/main/webapp/static/js/common.js
@@ -0,0 +1,10 @@
+const sendAjaxRequest = (method, url, data) => new Promise(resolve => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => resolve(xhr.responseText));
+    xhr.open(method, url);
+    if (method === 'GET') {
+        xhr.send();
+    } else {
+        xhr.send(data);
+    }
+})
