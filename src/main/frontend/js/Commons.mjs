c42bb5142aa8edf5af957e5b47f09e57cc9e65a5
E.Park <epark.grace@gmail.com>
2020-09-21 19:03:02 +0900

    refactor: common.js 모듈로 변경
    - 웹팩 엔트리에서 common.js 삭제
    - sendAjaxRequest를 Ajax 클래스로 변경
    - Commons.mjs로 파일명 변경

---
+++ src/main/frontend/js/Commons.mjs
@@ -0,0 +1,14 @@
+export class Ajax {
+    request(method, url, data) {
+        return new Promise(resolve => {
+            const xhr = new XMLHttpRequest();
+            xhr.addEventListener('load', () => resolve(xhr.responseText));
+            xhr.open(method, url);
+            if (method === 'GET') {
+                xhr.send();
+            } else {
+                xhr.send(data);
+            }
+        });
+    }
+}
