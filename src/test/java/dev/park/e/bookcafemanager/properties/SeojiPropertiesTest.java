213036782656e3b6f46ece5301a232ae150879fe
E.Park <epark.grace@gmail.com>
2021-03-29 18:15:52 +0900

    test: 서지정보 API 프로퍼티 테스트코드 추가
    - reference #34

---
+++ src/test/java/dev/park/e/bookcafemanager/properties/SeojiPropertiesTest.java
@@ -0,0 +1,29 @@
+package dev.park.e.bookcafemanager.properties;
+
+import dev.park.e.bookcafemanager.config.PropertiesConfiguration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = PropertiesConfiguration.class)
+@TestPropertySource("classpath:application.properties")
+class SeojiPropertiesTest {
+
+    @Autowired
+    SeojiProperties seojiProperties;
+
+    @Test
+    void 서지_키_조회() {
+        assertThat(seojiProperties.getCertKey()).isNotNull();
+        assertThat(seojiProperties.getResultStyle()).isNotNull();
+        assertThat(seojiProperties.getPageNo()).isNotNull();
+        assertThat(seojiProperties.getPageSize()).isNotNull();
+    }
+
+}
