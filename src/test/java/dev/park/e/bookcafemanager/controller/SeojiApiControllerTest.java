d347b6e031ca69afdb61f5488ab8aab90a2b8076
E.Park <epark.grace@gmail.com>
2021-04-01 00:03:59 +0900

    test: ISBN으로 서지정보 API 조회 기능 테스트코드 추가
    reference #34

---
+++ src/test/java/dev/park/e/bookcafemanager/controller/SeojiApiControllerTest.java
@@ -0,0 +1,52 @@
+package dev.park.e.bookcafemanager.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.park.e.bookcafemanager.dto.HttpResponseBody;
+import dev.park.e.bookcafemanager.dto.SeojiInfoDto;
+import dev.park.e.bookcafemanager.service.SeojiApiService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.nio.file.Paths;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(SeojiApiController.class)
+class SeojiApiControllerTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    SeojiApiService seojiApiService;
+
+    @Test
+    void isbn으로_서지정보_조회() throws Exception {
+        //given
+        String uri = "/api/seoji-info";
+        String isbn = "9791100000000";
+        SeojiInfoDto seojiInfoDto = objectMapper.readValue(Paths.get("src/test/resources/seoji-api-response.json").toFile(), SeojiInfoDto.class);
+        given(seojiApiService.getSeojiInfoByIsbn(isbn)).willReturn(seojiInfoDto);
+        HttpResponseBody body = new HttpResponseBody("조회되었습니다.", seojiInfoDto);
+
+        //when
+        ResultActions resultActions = mockMvc.perform(get(uri).param("isbn", isbn));
+
+        //then
+        resultActions.andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(body)))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+}
---
+++ src/test/java/dev/park/e/bookcafemanager/service/SeojiApiServiceTest.java
@@ -0,0 +1,65 @@
+package dev.park.e.bookcafemanager.service;
+
+import dev.park.e.bookcafemanager.dto.SeojiInfoDto;
+import dev.park.e.bookcafemanager.properties.SeojiProperties;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+@RestClientTest(SeojiApiService.class)
+@AutoConfigureWebClient(registerRestTemplate = true)
+@TestPropertySource("classpath:application.properties")
+class SeojiApiServiceTest {
+
+    @Autowired
+    MockRestServiceServer mockRestServiceServer;
+
+    @Autowired
+    SeojiApiService seojiApiService;
+
+    @Autowired
+    SeojiProperties properties;
+
+    @Test
+    void isbn으로_서지정보_조회() {
+        //given
+        String isbn = "9791100000000";
+        String response = "/seoji-api-response.json";
+        URI uri = UriComponentsBuilder.fromUriString("http://seoji.nl.go.kr/landingPage/SearchApi.do")
+                .queryParam("cert_key", properties.getCertKey())
+                .queryParam("result_style", properties.getResultStyle())
+                .queryParam("page_no", properties.getPageNo())
+                .queryParam("page_size", properties.getPageSize())
+                .queryParam("isbn", isbn)
+                .build().toUri();
+
+        mockRestServiceServer.expect(requestTo(uri))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withSuccess(new ClassPathResource(response), MediaType.APPLICATION_JSON));
+
+        //when
+        SeojiInfoDto seojiInfoDto = seojiApiService.getSeojiInfoByIsbn(isbn);
+
+        //then
+        assertThat(seojiInfoDto.getAuthor()).isEqualTo("작가");
+        assertThat(seojiInfoDto.getPublisher()).isEqualTo("출판사");
+        assertThat(seojiInfoDto.getTitle()).isEqualTo("제목");
+        assertThat(seojiInfoDto.getVolume()).isEqualTo(1);
+
+    }
+
+}
