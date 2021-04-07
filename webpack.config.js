1b43e75c095fc1d52f1b2ebd58c9ebecbb023ed2
E.Park <epark.grace@gmail.com>
2021-04-08 01:38:06 +0900

    chore: book-creation-form.js 번들링 빌드 코드 추가
    reference #34

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,9 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     mode: "development",
     entry: {
-        "book-list": './src/main/frontend/entry/book-list.js',
-        "shelf-editor": `./src/main/frontend/entry/shelf-editor.js`
+        "book-list": "./src/main/frontend/entry/book-list.js",
+        "shelf-editor": "./src/main/frontend/entry/shelf-editor.js",
+        "book-creation-form": "./src/main/frontend/entry/book-creation-form.js"
     },
     output: {
         filename: "[name].bundle.js",
