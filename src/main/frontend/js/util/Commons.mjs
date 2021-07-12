c7459dfe7e56435bc06cfdd6c493e70050cc4946
E.Park <epark.grace@gmail.com>
2021-07-12 13:48:36 +0900

    feat: Commons.mjs debounce 추가

--- src/main/frontend/js/util/Commons.mjs
+++ src/main/frontend/js/util/Commons.mjs
@@ -43,3 +43,13 @@ export class Message {
         }, 1000, wrapper);
     }
 }
+
+export function debounce(func, delay) {
+    let timer;
+    return (...args) => {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => func(...args), delay);
+    }
+}
